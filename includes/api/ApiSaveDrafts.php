254491e6508641e94cc527b747a80f411c4ef22c
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-04-28 11:25:23 +0200

    updated

--- includes/api/ApiSaveDrafts.php
+++ includes/api/ApiSaveDrafts.php
@@ -34,7 +34,7 @@ class ApiSaveDrafts extends ApiBase {
 		}
 		$draft->setToken( $params['drafttoken'] );
 		$draft->setTitle( Title::newFromText( $params['title'] ) );
-		$draft->setSection( $params['wpSection'] == '' ? null : $params['wpSection'] );
+		$draft->setSection( 0 );
 		$draft->setStartTime( $params['starttime'] );
 		$draft->setEditTime( $params['edittime'] );
 		$draft->setSaveTime( wfTimestampNow() );
