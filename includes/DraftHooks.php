02ae5fd5aca14cfb3e2bc77db8773d1543e08b05
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-04-28 17:40:47 +0200

    updated

--- includes/DraftHooks.php
+++ includes/DraftHooks.php
@@ -262,9 +262,8 @@ class DraftHooks {
 			$draft = Draft::newFromID($request->getInt('wpDraftApprove', 0));
 			if ($draft->exists()) {
 				self::$draftApprover = $user;
-				hSaveTest($editpage->textbox1);
 				$editPage->getContext()->setUser(User::newFromId($draft->getUserID()));
-				$editpage->textbox1 = $draft->getText();
+				$editPage->textbox1 = $draft->getText();
 			}
 		}
 	}
