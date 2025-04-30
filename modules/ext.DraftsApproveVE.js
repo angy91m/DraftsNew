500c528a78bfbe6ef4fd781fdccd48a75b7a9ffa
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-05-01 00:36:26 +0200

    updated

--- modules/ext.DraftsApproveVE.js
+++ modules/ext.DraftsApproveVE.js
@@ -1,14 +1,14 @@
 
 function changeSaveButtonTextMobile() {
     const txt = document.querySelector( '.oo-ui-processDialog-navigation .oo-ui-processDialog-actions-primary .oo-ui-labelElement-label' )?.innerText;
-    if (txt && txt !== 'Proponi') {
-        document.querySelector( '.oo-ui-processDialog-navigation .oo-ui-processDialog-actions-primary .oo-ui-labelElement-label' ).innerText = 'Proponi';
+    if (txt && txt !== mediaWiki.message( 'draftsapprove-view-propose' ).text()) {
+        document.querySelector( '.oo-ui-processDialog-navigation .oo-ui-processDialog-actions-primary .oo-ui-labelElement-label' ).innerText = mediaWiki.message( 'draftsapprove-view-propose' ).text();
     }
 }
 function changeSaveButtonText() {
     const saveButton = document.querySelector('.ve-ui-toolbar-group-save .oo-ui-tool-title');
     if (saveButton) {
-        saveButton.innerText = 'Proponi' + '...';
+        saveButton.innerText = mediaWiki.message( 'draftsapprove-view-propose' ).text() + '...';
     }
     new MutationObserver(changeSaveButtonTextMobile).observe(document.querySelector('#mw-teleport-target .ve-ui-overlay-global.ve-ui-overlay-global-mobile.ve-ui-overlay'), {childList: true, subtree: true});
 }
