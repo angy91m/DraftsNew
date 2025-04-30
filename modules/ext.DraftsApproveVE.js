b27c54bc6f75cbc2bc0890957d593daa6e7f9df7
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-04-30 23:57:31 +0200

    updated

--- modules/ext.DraftsApproveVE.js
+++ modules/ext.DraftsApproveVE.js
@@ -1,8 +1,13 @@
 function changeSaveButtonText() {
-    // Cerca il bottone "Salva pagina..."
     const saveButton = document.querySelector('.ve-ui-toolbar-group-save .oo-ui-tool-title');
     if (saveButton) {
         saveButton.textContent = 'Proponi';
     }
 }
-mw.hook('ve.activationComplete').add(changeSaveButtonText);
+function changeSaveButtonTextMobile() {
+    console.log('ciao');
+}
+mw.hook( 've.newTarget' ).add( ( target ) => {
+    target.on( 'surfaceReady', changeSaveButtonText );
+    target.on( 'saveWorkflowChangePanel', changeSaveButtonTextMobile );
+} );
