3666f8519d521ba6b051709c7a9fd4ce6afacb3d
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-05-02 20:41:46 +0200

    updated

--- modules/ext.DraftsApproveVE.js
+++ modules/ext.DraftsApproveVE.js
@@ -27,10 +27,10 @@ function onActivation() {
         if (data.errors && Array.isArray(data.errors) && data.errors.length) {
             const error = data.errors[0];
             if (error.data?.edit?.redirectTarget) {
-                setTimeout(() => {
-                    window.onbeforeunload = null;
-                    location.href = error.data.edit.redirectTarget;
-                }, 3000);
+                // setTimeout(() => {
+                //     window.onbeforeunload = null;
+                //     location.href = error.data.edit.redirectTarget;
+                // }, 3000);
             }
         }
     };
