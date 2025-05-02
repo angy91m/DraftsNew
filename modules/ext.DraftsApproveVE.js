b391288f105d4495fcf883f9e0290ade698e02c3
Angelo Burzi <angelo.burzi@immobiliare.it>
2025-05-02 20:52:07 +0200

    updated

--- modules/ext.DraftsApproveVE.js
+++ modules/ext.DraftsApproveVE.js
@@ -30,9 +30,8 @@ function onActivation() {
                 setTimeout(() => {
                     document.querySelector('.oo-ui-processDialog-errors-actions').style.display = 'none';
                     document.querySelector('.oo-ui-processDialog-errors .oo-ui-flaggedElement-error').classList.replace('oo-ui-flaggedElement-error', 'oo-ui-flaggedElement-success');
-                    const icon = document.querySelector('.oo-ui-processDialog-errors .oo-ui-icon-error.oo-ui-image-error');
-                    icon.classList.replace('.oo-ui-icon-error', '.oo-ui-icon-success');
-                    icon.classList.replace('.oo-ui-image-error', '.oo-ui-image-success');
+                    document.querySelector('.oo-ui-processDialog-errors .oo-ui-icon-error.oo-ui-image-error').classList.replace('.oo-ui-icon-error', '.oo-ui-icon-success');
+                    document.querySelector('.oo-ui-processDialog-errors .oo-ui-icon-success.oo-ui-image-error').classList.replace('.oo-ui-image-error', '.oo-ui-image-success');
                 }, 0);
                 // setTimeout(() => {
                 //     window.onbeforeunload = null;
